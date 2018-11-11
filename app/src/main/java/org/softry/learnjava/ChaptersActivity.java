40ec966dc25d7a0ef0232f8761c44087a229f327
Andrei Ciucur <ciucur.andrei@gmail.com>
2018-11-11 15:13:32 +0200

    Implement RV adapter for chapter select activity

--- app/src/main/java/org/softry/learnjava/ChaptersActivity.java
+++ app/src/main/java/org/softry/learnjava/ChaptersActivity.java
@@ -1,13 +1,49 @@
 package org.softry.learnjava;
 
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class ChaptersActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChaptersActivity extends AppCompatActivity implements RVA_Chapters.ItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapters);
+
+        List<RVA_Chapters.Chapter> mChapters = new ArrayList<>();
+        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter1_title), getResources().getString(R.string.chapter1_name), getResources().getString(R.string.chapter1_desc)));
+        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter2_title), getResources().getString(R.string.chapter2_name), getResources().getString(R.string.chapter2_desc)));
+
+        //Set Recycler View divider
+        DividerItemDecoration divItemDec = new DividerItemDecoration(getBaseContext(),DividerItemDecoration.VERTICAL);
+        divItemDec.setDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.rv_divider));
+
+        //Set Recycler View
+        RecyclerView recyclerView = findViewById(R.id.rvChapters);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(divItemDec);
+
+        //Set Recycler View adapter
+        RVA_Chapters rvAdapter;
+        rvAdapter = new RVA_Chapters(this, mChapters);
+        rvAdapter.setClickListener(this);
+        recyclerView.setAdapter(rvAdapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        /*Intent readThisEntry = new Intent(this, ReadJournal.class);
+        String entryUid = rvAdapter.getItemUID(position);
+        readThisEntry.putExtra(ENTRY_UID_KEY, entryUid);
+        startActivity(readThisEntry);*/
     }
 }
---
+++ app/src/main/java/org/softry/learnjava/RVA_Chapters.java
@@ -0,0 +1,113 @@
+package org.softry.learnjava;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RVA_Chapters extends RecyclerView.Adapter<RVA_Chapters.mViewHolder> {
+
+    public static class Chapter {
+        private String num;
+        private String name;
+        private String desc;
+        private int progress;
+
+        public Chapter(String num, String name, String desc) {
+            this.num = num;
+            this.name = name;
+            this.desc = desc;
+            this.progress = 0;
+        }
+
+        public String Num(){
+            return this.num;
+        }
+
+        public String Name() {
+            return this.name;
+        }
+
+        public String Desc() {
+            return this.desc;
+        }
+
+        public String Progress() {
+            return Integer.toString(this.progress) + "%";
+        }
+    }
+
+    private List<Chapter> mChapters;
+
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    public class mViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView chapterNumber, chapterName, chapterDesc, chapterProgress;
+
+        public mViewHolder(View itemView) {
+            super(itemView);
+            chapterNumber = itemView.findViewById(R.id.tvChapterNum);
+            chapterName = itemView.findViewById(R.id.tvChapterName);
+            chapterDesc = itemView.findViewById(R.id.tvChapterDesc);
+            chapterProgress = itemView.findViewById(R.id.tvChapterProgress);
+            itemView.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View view) {
+
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    RVA_Chapters(Context context, List<Chapter> entryObjList) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mChapters = entryObjList;
+    }
+
+    @Override
+    public mViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.layout_chapter, parent,false);
+        return new mViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(mViewHolder holder, int position) {
+        Chapter itemElem = mChapters.get(position);
+
+        holder.chapterNumber.setText(itemElem.Num());
+        holder.chapterName.setText(itemElem.Name());
+        holder.chapterDesc.setText(itemElem.Desc());
+        holder.chapterProgress.setText(itemElem.Progress());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mChapters.size();
+    }
+
+
+    /*String getEntryText(int id) {
+        return mEntryObjList.get(id).getText();
+    }
+
+    String getEntryTime(int id) {
+        return mEntryObjList.get(id).getTimestamp();
+    }*/
+
+
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
