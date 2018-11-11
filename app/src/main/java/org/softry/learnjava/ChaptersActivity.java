de67d3bb720efe8edc770ce06310d938433ab67f
Andrei Ciucur <ciucur.andrei@gmail.com>
2018-11-11 16:10:14 +0200

    Separate RV setup into methods

--- app/src/main/java/org/softry/learnjava/ChaptersActivity.java
+++ app/src/main/java/org/softry/learnjava/ChaptersActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -14,33 +15,74 @@ import java.util.List;
 
 public class ChaptersActivity extends AppCompatActivity implements RVA_Chapters.ItemClickListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chapters);
+    private List<RVA_Chapters.Chapter> mChapters;
 
-        List<RVA_Chapters.Chapter> mChapters = new ArrayList<>();
-        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter1_title), getResources().getString(R.string.chapter1_name), getResources().getString(R.string.chapter1_desc)));
-        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter2_title), getResources().getString(R.string.chapter2_name), getResources().getString(R.string.chapter2_desc)));
+    private RecyclerView mRecyclerView;
+
+    private void SetChapterList() {
+        mChapters = new ArrayList<>();
+        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter1_title),
+                getResources().getString(R.string.chapter1_name),
+                getResources().getString(R.string.chapter1_desc)));
+
+        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter2_title),
+                getResources().getString(R.string.chapter2_name),
+                getResources().getString(R.string.chapter2_desc)));
+
+        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter3_title),
+                getResources().getString(R.string.chapter3_name),
+                getResources().getString(R.string.chapter3_desc)));
+
+        mChapters.add(new RVA_Chapters.Chapter(getResources().getString(R.string.chapter4_title),
+                getResources().getString(R.string.chapter4_name),
+                getResources().getString(R.string.chapter4_desc)));
+    }
+
+    private RVA_Chapters GetChaptersRVA() {
+        RVA_Chapters rvAdapter;
+        if(this.mChapters == null) return null;
+
+        rvAdapter = new RVA_Chapters(this, this.mChapters);
+        rvAdapter.setClickListener(this);
+        return rvAdapter;
+    }
+
+    //TODO: Create exception for null adapter
+    private void InitRV_Chapters() throws Exception{
+        RVA_Chapters adapter = GetChaptersRVA();
+        if (adapter == null) {
+            throw new Exception("Null adapter. Call SetChapterList() first.");
+        }
 
         //Set Recycler View divider
         DividerItemDecoration divItemDec = new DividerItemDecoration(getBaseContext(),DividerItemDecoration.VERTICAL);
         divItemDec.setDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.rv_divider));
 
-        //Set Recycler View
-        RecyclerView recyclerView = findViewById(R.id.rvChapters);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addItemDecoration(divItemDec);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(divItemDec);
+        mRecyclerView.setAdapter(adapter);
+    }
 
-        //Set Recycler View adapter
-        RVA_Chapters rvAdapter;
-        rvAdapter = new RVA_Chapters(this, mChapters);
-        rvAdapter.setClickListener(this);
-        recyclerView.setAdapter(rvAdapter);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapters);
+
+        mRecyclerView = findViewById(R.id.rvChapters);
+
+        SetChapterList();
+
+        try {
+            InitRV_Chapters();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onItemClick(View view, int position) {
+        Log.i("myapp_info", "test");
+
         /*Intent readThisEntry = new Intent(this, ReadJournal.class);
         String entryUid = rvAdapter.getItemUID(position);
         readThisEntry.putExtra(ENTRY_UID_KEY, entryUid);
